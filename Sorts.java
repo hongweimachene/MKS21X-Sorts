3d957179516f21f1de119b1b9717055ccd4fe42e
hongweimachene <hchen00@stuy.edu>
2018-12-04 23:50:26 -0500

    add to string to test

--- Sorts.java
+++ Sorts.java
@@ -6,12 +6,29 @@ public class Sorts{
       int index = 0;
       for (int j = i; j < ary.length; j++) {
         if (ary[j] < small) {
-
+          index = j;
           small = ary[j];
         }
       }
       ary[i] = small;
-      ary[]
+      ary[index] = store;
     }
+  }
+  public String toString(int[] ary) {
+    String s = "";
+    for (int i = 0; i < ary.length; i++) {
+      if (i == ary.length - 1) {
+        s+=ary[i];
+      } else {
+        s+= ary[i]+", ";
+      }
+    }
+    return s;
+  }
+  public static void main(String[] args) {
+    int[] ary = new int[] {6,3,9,2,2,1,5};
+    System.out.println(toString(ary));
+    selectionsort(ary);
+    System.out.println(toString(ary));
   }
 }
