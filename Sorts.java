80eec6bb48f62673c28288c1c59158cb534c0450
hongweimachene <hchen00@stuy.edu>
2018-12-04 23:44:05 -0500

    1

---
+++ Sorts.java
@@ -0,0 +1,17 @@
+public class Sorts{
+  public static void selectionsort(int[] ary) {
+    for (int i = 0; i < ary.length; i++) {
+      int small = ary[i];
+      int store = small;
+      int index = 0;
+      for (int j = i; j < ary.length; j++) {
+        if (ary[j] < small) {
+
+          small = ary[j];
+        }
+      }
+      ary[i] = small;
+      ary[]
+    }
+  }
+}
